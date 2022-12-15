08b1588949fdc393d3f8f67d97d17631226bbecc
Maximilian Radtke <75415986+maxmilianr@users.noreply.github.com>
2022-12-15 14:42:41 +0100

    Update ERCC expression analysis.R

--- ERCC expression analysis.R
+++ ERCC expression analysis.R
@@ -1,47 +1,38 @@
 ##ERCC spike in RNA analysis
 
 library(DESeq2)
+library(ReportingTools)
 
 #read the ERCC sample table, "sample_sheet_ercc.txt"
-# add a comment
-
-
 sampletable <- read.table('sample_sheet_ercc.txt', header=T, sep="\t") # add the path to sample_table.txt here
 
 #create a DESeq DataSet object "ercc_dd", designed over the variable Mix
-
 ercc_dd <- DESeqDataSetFromHTSeqCount(sampleTable = sampletable,
-                                      directory = "G:/RNAseq analysis/analysis01/ercc",
+                                      directory = "ercc_counts",
                                       design = ~ Mix)
 
 #create an object "ercc_dds" with DESeq2 normalized expression   
-
 ercc_dds <- DESeq(ercc_dd)
 
 #create the object "res_ercc", with the ratio of Mix2 vs Mix1
-
 res_ercc <- results(object = ercc_dds)
-
 write.table(res_ercc, "ercc2_vs_ercc1_deseq2_results.txt", quote=F, col.names=T, row.names=T, sep="\t") 
 
 #visualize the results of ercc_dds object,
-
-library(ReportingTools)
-
 des2Report <- HTMLReport(shortName = 'ERCC',
                          title = 'ERCC',
                          reportDirectory = "reports")
 
 colData(ercc_dds)$Mix <- factor(colData(ercc_dds)$Mix, levels=c("M1", "M2"))
-publish(ercc_dds,des2Report, pvalueCutoff=1,
-        annotation.db=NULL, factor=colData(ercc_dds)$Mix,
+publish(ercc_dds,des2Report, 
+        pvalueCutoff=1,
+        annotation.db=NULL, 
+        factor=colData(ercc_dds)$Mix,
         reportDir="./reports")
+
 finish(des2Report)
 
-
-
 #create heatmap for the spike in RNA
-
 library(pheatmap)
 norm_counts <- log2(counts(ercc_dds, normalized = TRUE)+1)
 exp_sel_heatmap <- pheatmap(norm_counts, cluster_cols = FALSE, legend = TRUE)
