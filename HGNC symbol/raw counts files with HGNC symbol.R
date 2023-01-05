a3da97cde63ef6a9082383589b9193981c41d221
harishharish759 <harishharish759@gmail.com>
2023-01-05 10:51:47 +0100

    updated the files

--- HGNC symbol/raw counts files with HGNC symbol.R
+++ HGNC symbol/raw counts files with HGNC symbol.R
@@ -1,12 +1,16 @@
+#to create new raw count files with HGNC symbol from raw count files with ensembl ID#
+
+#load the required libraries 
+
 library(biomaRt)
+library(stringr)
 
-#to create raw count files with HGNC symbol from raw count files with ensembl ID#
+#read the ensembl ID of transcripts present in raw counts file (found in folder "Raw counts/ercc_ens_counts")
 
-#read the ensembl ID of transcripts present in raw counts file
-
-ensembl_ids <- read.csv("2-1.ReadsPerGeneCol4.tab", sep="\t", header = FALSE)[,1]
+ensembl_ids <- read.csv("Raw counts/ercc_ens_counts/2-1.ReadsPerGeneCol4.tab", sep="\t", header = FALSE)[,1]
 
 #create a data frame of HGNC symbol and the respective ensembl ID
+
 mart <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
 attributes=c('ensembl_gene_id','hgnc_symbol')
 
@@ -15,13 +19,11 @@ ens_hgnc_list <- getBM(attributes=attributes, mart=mart, filters="ensembl_gene_i
 ens_hgnc_list<- data.frame(ens_hgnc_list)
 
 
-#loop over all files and replace ensembl ID by HGNC symbol and save the files in a new folder "hg38"
+#loop over all files and replace ensembl ID by HGNC symbol and save the new files in a folder "hg38_counts"
 
-ensembl_count_files <- list.files(path="counts/ensembl_id/", pattern="*.tab", full.names=TRUE, recursive=FALSE)
+ensembl_count_files <- list.files(path="Raw counts/ercc_ens_counts/", pattern="*.tab", full.names=TRUE, recursive=FALSE)
 
-library(stringr)
-
-dir.create("hg38")
+dir.create("Raw counts/hg38_counts")
 
 lapply(ensembl_count_files, function(x) {
   
@@ -36,8 +38,6 @@ lapply(ensembl_count_files, function(x) {
   df_ens_counts_merge.file.clean <- df_ens_counts_merge.file[!(df_ens_counts_merge.file$hgnc_symbol==""),]
   
   
-  hgnc_filename <- str_replace(x, "counts/ensembl_id/", "" )
-  write.table(df_ens_counts_merge.file.clean, paste("counts/hg38/",hgnc_filename, sep=""), row.names = FALSE, col.names = FALSE, sep = "\t", quote=FALSE)
+  hgnc_filename <- str_replace(x, "Raw counts/ercc_ens_counts/", "" )
+  write.table(df_ens_counts_merge.file.clean, paste("Raw counts/hg38_counts/",hgnc_filename, sep=""), row.names = FALSE, col.names = FALSE, sep = "\t", quote=FALSE)
 })
-
-#####
