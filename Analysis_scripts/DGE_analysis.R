44b097763030dfbd350cbee8dd4c775a1ae31c69
harishharish759 <harishharish759@gmail.com>
2023-01-05 16:26:50 +0100

    Update DGE_analysis.R

--- Analysis_scripts/DGE_analysis.R
+++ Analysis_scripts/DGE_analysis.R
@@ -8,7 +8,7 @@ library(DESeq2)
 
 sampletable <- read.table('Raw counts/sample_sheet.txt', header=T, sep="\t")  #set path for sample table
 
-#load the raw counts files (created in the in the using the script 'raw counts files with HGNC symbol.R' )
+#load the raw counts files (created using the script 'raw counts files with HGNC symbol.R' )
 
 se_star <- DESeqDataSetFromHTSeqCount(sampleTable = sampletable,
                                       directory = "Raw counts/hg38_counts",
@@ -16,8 +16,9 @@ se_star <- DESeqDataSetFromHTSeqCount(sampleTable = sampletable,
 
 se_star2 <- DESeq(se_star)
 
-#write results of DGE analysis for two time points and save as text file
+#write results of DGE analysis for selected two time points and save as text file
+# you can adjust the timepoints to compare here, i.e. Time_t2_vs_t1, Time_t3_vs_t1, etc
 
-res <- results(object = se_star2, name="Time_t6_vs_t1") # you can adjust the timepoints to compare here, i.e. Time_t2_vs_t1, Time_t3_vs_t1, etc
+res <- results(object = se_star2, name="Time_t6_vs_t1") 
 
 write.table(res, "t6_vs_t1_deseq2_results.txt", quote=F, col.names=T, row.names=T, sep="\t") 
