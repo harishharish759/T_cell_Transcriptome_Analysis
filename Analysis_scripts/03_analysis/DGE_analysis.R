a662948faf20d526c8016118f099acf2c210d7af
harishharish759 <harishharish759@gmail.com>
2023-08-17 01:54:28 +0200

    Update DGE_analysis.R

--- Analysis_scripts/03_analysis/DGE_analysis.R
+++ Analysis_scripts/03_analysis/DGE_analysis.R
@@ -8,17 +8,21 @@ library(DESeq2)
 
 sampletable <- read.table('Raw counts/sample_sheet.txt', header=T, sep="\t")  #set path for sample table
 
-#load the raw counts files (created using the script 'raw counts files with HGNC symbol.R' )
+#load the raw counts files (created using the script 'raw counts files with HGNC symbol.R' ) and build the DESeq dataset
 
 se_star <- DESeqDataSetFromHTSeqCount(sampleTable = sampletable,
                                       directory = "Raw counts/hg38_counts",
                                       design = ~ Time)
 
+#perform differential gene expression analysis
+
 se_star2 <- DESeq(se_star)
 
-#write results of DGE analysis for selected two time points and save as text file
-# you can adjust the timepoints to compare here, i.e. Time_t2_vs_t1, Time_t3_vs_t1, etc
 
-res <- results(object = se_star2, name="Time_t6_vs_t1") 
+#perfrom lfcshrink and write results of DGE analysis for two selected time points and save as text file
 
-write.table(res, "t6_vs_t1_deseq2_results.txt", quote=F, col.names=T, row.names=T, sep="\t") 
+resultsNames(se_star2)  #provides the name  of the coefficient for results, i.e. Time_t2_vs_t1, Time_t3_vs_t1, etc
+
+resLFC <- lfcShrink(se_star2, coef = "Time_t6_vs_t1" , type = "apeglm") 
+
+write.table(resLFC, "t6_vs_t1_deseq2_results.txt", quote=F, col.names=T, row.names=T, sep="\t") 
